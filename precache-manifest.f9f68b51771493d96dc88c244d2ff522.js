d66dddc67b4104375449f4e4900a3ec8fe3f1b27
AndreaCanevaro <andrea.canevaro.f@gmail.com>
2023-01-23 13:35:28 -0300

    Updates

---
+++ precache-manifest.f9f68b51771493d96dc88c244d2ff522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5a77cb26ee4d8b1bf2c",
+    "url": "/static/css/main.1a9e4911.chunk.css"
+  },
+  {
+    "revision": "e5a77cb26ee4d8b1bf2c",
+    "url": "/static/js/main.e5a77cb2.chunk.js"
+  },
+  {
+    "revision": "e0f3e693a15b96a52fa7",
+    "url": "/static/js/1.e0f3e693.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a4adb124415311e84f7411b45462cc8b",
+    "url": "/index.html"
+  }
+];
